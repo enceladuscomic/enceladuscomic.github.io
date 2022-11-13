1345209abeff5a2ea3bdd642614b88904355c31a
Jeremy Hall <jeremyhall9804@gmail.com>
2022-11-13 01:15:28 -0500

    landing eye, rm assets

---
+++ index4.js
@@ -0,0 +1,7 @@
+function ani() {
+    let el = document.getElementById("eyelid-span");
+    el.style.animation = "1s ease-in eye-open-animation";
+    el.style.height = "0%";
+    // document.getElementById("eyelid-span").style.animation = "1s ease-in eye-open-animation";
+    // document.getElementById("eyelid-span").style.height = "0%";
+};
