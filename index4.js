2abca5805e3eee68f94444bbe68d1c16ace3a018
Jeremy Hall <jeremyhall9804@gmail.com>
2022-11-13 01:36:51 -0500

    open/close eye

--- index4.js
+++ index4.js
@@ -1,7 +1,18 @@
+let isOpen = false;
+
 function ani() {
+    console.log("clicked");
     let el = document.getElementById("eyelid-span");
-    el.style.animation = "1s ease-in eye-open-animation";
-    el.style.height = "0%";
+    if (!isOpen) {
+        el.style.animation = "1s ease-in eye-open-animation";
+        el.style.height = "0%";
+        isOpen = true;
+    }
+    else if (isOpen) {
+        el.style.animation = "1s ease-in eye-close-animation";
+        el.style.height = "100%";
+        isOpen = false;
+    }
     // document.getElementById("eyelid-span").style.animation = "1s ease-in eye-open-animation";
     // document.getElementById("eyelid-span").style.height = "0%";
 };
