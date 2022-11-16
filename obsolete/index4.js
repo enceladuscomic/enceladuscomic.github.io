ea7e83aac90c0b099e0b2dd5a17ee4879ca8160e
Jeremy Hall <jeremyhall9804@gmail.com>
2022-11-15 23:39:25 -0500

    mobile landing complete

---
+++ obsolete/index4.js
@@ -0,0 +1,17 @@
+let isOpen = false;
+
+function ani() {
+    let el = document.getElementById("eyelid-span");
+    if (!isOpen) {
+        el.style.animation = "1s ease-in eye-open-animation";
+        el.style.height = "0%";
+        isOpen = true;
+    }
+    else if (isOpen) {
+        el.style.animation = "1s ease-in eye-close-animation";
+        el.style.height = "100%";
+        isOpen = false;
+    }
+    // document.getElementById("eyelid-span").style.animation = "1s ease-in eye-open-animation";
+    // document.getElementById("eyelid-span").style.height = "0%";
+};
